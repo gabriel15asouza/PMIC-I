25a2b45af3d2cab6f1465cc26f2a5a71a2e030c0
gabriel15asouza <gabalves.souza@gmail.com>
2023-05-05 07:28:24 -0300

    Introducing database
    Adding a database linked to page 'contatos'

--- Desafio 3/app.py
+++ Desafio 3/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask (__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234' 
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -13,3 +21,20 @@ def contato():
 @app.route('/quemsomos')
 def quemsomos():
     return render_template('quemsomos.html')
+
+@app.route('/contatos', methods=['GET','POST'])
+def contatos():
+		if request.method == 'POST':
+				email = request.form['email']
+				assunto = request.form['assunto']
+				descricao = request.form['descricao']
+
+				cur = mysql.connection.cursor()
+				cur.execute(f'INSERT INTO CONTATO (EMAIL,ASSUNTO,DESCRICAO) VALUES ({email},{assunto},{descricao})')
+				
+				mysql.commit()
+
+				cur.close()
+
+				return 'Sucesso'
+				
