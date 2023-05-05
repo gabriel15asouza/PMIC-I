3803e0960593b02aa954143a18f7d2327972c905
gabriel15asouza <gabalves.souza@gmail.com>
2023-05-05 08:50:53 -0300

    Corrected database
    Updated routes and corrected mistakes in app.py and page 'contatos'

--- Desafio 3/app.py
+++ Desafio 3/app.py
@@ -6,17 +6,13 @@ app = Flask (__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234' 
-app.config['MYSQL_DB'] = 'contatos'
+app.config['MYSQL_DB'] = 'Desafio3'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
 
 @app.route('/quemsomos')
 def quemsomos():
@@ -25,16 +21,27 @@ def quemsomos():
 @app.route('/contatos', methods=['GET','POST'])
 def contatos():
 		if request.method == 'POST':
-				email = request.form['email']
-				assunto = request.form['assunto']
-				descricao = request.form['descricao']
+			email = request.form['email']
+			assunto = request.form['assunto']
+			descricao = request.form['descricao']
 
-				cur = mysql.connection.cursor()
-				cur.execute(f'INSERT INTO CONTATO (EMAIL,ASSUNTO,DESCRICAO) VALUES ({email},{assunto},{descricao})')
+			cur = mysql.connection.cursor()
+			cur.execute('INSERT INTO CONTATOS (EMAIL,ASSUNTO,DESCRICAO) VALUES (%s,%s,%s)', (email, assunto, descricao))
 				
-				mysql.commit()
+			mysql.connection.commit()
 
-				cur.close()
+			cur.close() 	 	
 
-				return 'Sucesso'
-				
+			return 'Sucesso'
+		return render_template('contatos.html')
+
+@app.route('/users')
+def users():
+	cur = mysql.connection.cursor()
+
+	users = cur.execute("SELECT * FROM contatos")
+
+	if users > 0:
+		userDetails = cur.fetchall()
+
+	return render_template("users.html", userDetails=userDetails)
